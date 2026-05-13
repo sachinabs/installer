102b8bbb3ba2aa98e3c5bd1937e59d9d9e011489
Anish Bala Sachin <61160377+sachinabs@users.noreply.github.com>
2026-05-13 16:09:42 +0530

    ise

--- dbgate.sh
+++ dbgate.sh
@@ -1,3 +1,6 @@
 wget https://github.com/dbgate/dbgate/releases/latest/download/dbgate-latest.deb
 
 sudo apt install ./dbgate-latest.deb
+# sandbox issue
+sudo chown root:root /opt/DbGate/chrome-sandbox
+sudo chmod 4755 /opt/DbGate/chrome-sandbox
